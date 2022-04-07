7d9025b1ed3e1947f6c9acf60fd70dc242d7bf65
Felipe Bouilly <felipejosebouilly@gmail.com>
2022-04-07 16:21:05 +0200

    Initial repo

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+let saludo= "hola"
+let numero=3
+let decimal=3.3
+console.log("Saludo", saludo)
+
+let object={
+    prop:"nombrePropiedad",
+    otraProp:3,
+}
+console.log("Objeto",object)
+console.log("Propiedad",object.prop)
+
+let array=[1,3,4,5]
+console.log("array",array)
+console.log("arrayPos",array[1])
+
+let arrayObject=[
+    {
+        nombreLibro:"AlgunNombre",
+        Autor:"NombreAutor"
+    },
+    {
+        nombreLibro:"AlgunNombre2",
+        Autor:"NombreAutor2"
+    },
+]
+console.log("NombreLibro",arrayObject[0].nombreLibro)
+
+
+let objetoContArray={
+    librerias:["lib1","lib2"]
+}
+console.log("objetoArray",objetoContArray.librerias[1])
+
+
+console.log(primerIndice,segundoIndice,tercerIndice)
+console log("hola mundo")
